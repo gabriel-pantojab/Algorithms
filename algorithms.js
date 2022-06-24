8c6ad88ce66c3096a9fd2c07e3ecd1bf08319707
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2022-06-24 18:58:42 -0400

    implementacion binary-search, iterativo y recursivo

--- algorithms.js
+++ algorithms.js
@@ -150,6 +150,45 @@ console.log("Desordenado:",l4);
 insertionSortR(l4,l4.length);
 console.log("Ordenada:",l4);
 console.log();
+
+/**
+ *  BINARY SEARCH
+ *  Loop Invariant
+ *  Complex -> O(lg(n))
+ * 
+ *  La lista debe estar ordenada.
+ */
+function binarySearchI(v, array) {
+	let a,b,m;
+	a = 0;
+	b = array.length - 1;
+	while(a < b) {
+		m = Math.floor((a+b)/2);
+		if(array[m] < v) a = m + 1;
+		else b = m;
+	}
+	return array[a] === v ? a : -1;
+}
+
+function binarySearchR(v, array) {
+	return binarySearchRA(v,array,0,array.length-1);
+}
+function binarySearchRA(v, A, a, b) {
+	if(a < b) {
+		let m = Math.floor((a+b)/2);
+		if(A[m] < v) {
+			return binarySearchRA(v,A,m+1,b);
+		}
+		else return binarySearchRA(v,A,a,m);
+	}
+	return A[a] === v ? a : -1;
+}
+let l5 = [1,2,3,4,5,6];
+console.log("BINARY SEARCH")
+console.log("Lista:",l5);
+console.log("Elemento a buscar:",5);
+console.log("Posicion:",binarySearchR(5,l5));
+console.log();
 //**********************************************************************************************//
 
 
