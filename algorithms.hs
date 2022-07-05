ab855aaf068ac5bd01dc3b2c429ef29cea75a45f
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2022-07-04 20:17:35 -0400

    implementacion linear-search

--- algorithms.hs
+++ algorithms.hs
@@ -14,3 +14,14 @@ insertionSort (x:xs) = insert x (insertionSort xs)
 
 list = [3,1,4,2,5];
 
+{-
+	LINEAR SEARCH
+-}
+
+linearSearch :: (Eq a) => a -> [a] -> Int
+linearSearch v xs = lS v xs 0
+  where
+    lS v [] p = -1
+    lS v (x:xs) p
+      | v == x = p
+      | otherwise = lS v xs (p+1)
